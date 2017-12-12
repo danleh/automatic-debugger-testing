53bb30ab2de2e9e5dd7b26cee72c1a17866906d8
Daniel Lehmann <daniel.lehmann@stud.tu-darmstadt.de>
2017-12-12 19:55:44 +0100

    remove obsolete trace evaluation program (Scala)

--- evaluate-trace-logs/build.sbt
+++
@@ -1,8 +0,0 @@
-name := "evaluate-trace-logs"
-
-version := "1.0"
-
-scalaVersion := "2.12.1"
-
-libraryDependencies += "com.sksamuel.diff" % "diff" % "1.1.11"
-libraryDependencies += "com.github.tototoshi" %% "scala-csv" % "1.3.4"
--- evaluate-trace-logs/src/main/scala/eu/dlehmann/masterthesis/util/Outputs.scala
+++
@@ -1,5 +0,0 @@
-package eu.dlehmann.masterthesis.util
-
-case class Outputs(linesSum: Int, linesDiff: Int, linesDiffByLinesSum: Double) extends DataClass {
-  require(linesDiffByLinesSum >= 0.0 && linesDiffByLinesSum <= 1.0, s"percent not in interval [0, 1], was $linesDiffByLinesSum")
-}
