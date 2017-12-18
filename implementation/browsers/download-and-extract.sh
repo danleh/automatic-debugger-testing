5849aa54a0f8ea8e60be7f116ab1d9414d8a5c2e
Daniel Lehmann <daniel.lehmann@crisp-da.de>
2017-12-18 14:55:44 +0100

    add comments on where to get old releases of the browsers manually

--- implementation/browsers/download-and-extract.sh
+++ implementation/browsers/download-and-extract.sh
@@ -1,6 +1,8 @@
+# see https://superuser.com/questions/936432/how-do-i-install-a-previous-version-of-chrome
 curl https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F464640%2Fchrome-linux.zip?alt=media > chromium-59.zip
 unzip chromium-59.zip
 mv chrome-linux/ chromium-59/
+# from https://ftp.mozilla.org/pub/firefox/releases/
 curl https://ftp.mozilla.org/pub/firefox/releases/54.0/linux-x86_64/en-US/firefox-54.0.tar.bz2 > firefox-54.tar.bz2
 tar -xvf firefox-54.tar.bz2
 mv firefox/ firefox-54/
